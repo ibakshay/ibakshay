d9156b0c11c759772b8cf8b4df64b8123661d357
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2020-10-20 20:48:51 +0200

    Updates

---
+++ precache-manifest.e109a61d1654eac70c7b5c736d66ea47.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d48f5f1d051d897d395dba84880f70a",
+    "url": "/ibakshayportfolio/index.html"
+  },
+  {
+    "revision": "ea07cf7ca6787bfd87cb",
+    "url": "/ibakshayportfolio/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "bab2cacdb1d5a27c903d",
+    "url": "/ibakshayportfolio/static/css/4.a922a401.chunk.css"
+  },
+  {
+    "revision": "9a6111e1f52724b8c60c",
+    "url": "/ibakshayportfolio/static/css/main.1a06f89f.chunk.css"
+  },
+  {
+    "revision": "6b07b9e69d48d4f53504",
+    "url": "/ibakshayportfolio/static/js/2.6ab7f62b.chunk.js"
+  },
+  {
+    "revision": "ea07cf7ca6787bfd87cb",
+    "url": "/ibakshayportfolio/static/js/3.47efe438.chunk.js"
+  },
+  {
+    "revision": "bab2cacdb1d5a27c903d",
+    "url": "/ibakshayportfolio/static/js/4.fc537a76.chunk.js"
+  },
+  {
+    "revision": "9a6111e1f52724b8c60c",
+    "url": "/ibakshayportfolio/static/js/main.6257d962.chunk.js"
+  },
+  {
+    "revision": "01a2b9166c084806b7f5",
+    "url": "/ibakshayportfolio/static/js/runtime-main.7bbbb119.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/ibakshayportfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "7670707671a54acb44658ccde51d3d8c",
+    "url": "/ibakshayportfolio/static/media/CognizantLogo400.76707076.jpg"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/ibakshayportfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "4708f38c05d6ab10a0ef09bc37c70a3c",
+    "url": "/ibakshayportfolio/static/media/SAP-Logo2.4708f38c.png"
+  },
+  {
+    "revision": "a2709541036032cbccc4a3e0fc031eb0",
+    "url": "/ibakshayportfolio/static/media/SAP-logo-square.a2709541.png"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/ibakshayportfolio/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/ibakshayportfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/ibakshayportfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/ibakshayportfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/ibakshayportfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/ibakshayportfolio/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "/ibakshayportfolio/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/ibakshayportfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "83a05b10639d238bd8c5bbab279ab911",
+    "url": "/ibakshayportfolio/static/media/harvardLogo.83a05b10.png"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "/ibakshayportfolio/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ibakshayportfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/ibakshayportfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/ibakshayportfolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/ibakshayportfolio/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "1a60e8a9e2c2d6a539c7692f27fe6790",
+    "url": "/ibakshayportfolio/static/media/pwa.1a60e8a9.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/ibakshayportfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/ibakshayportfolio/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/ibakshayportfolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "a38bcf529e25592e2088fa3d581e557d",
+    "url": "/ibakshayportfolio/static/media/sap_logo.a38bcf52.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/ibakshayportfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "80d4b34c87c326b32e64a92bbd0cb1dd",
+    "url": "/ibakshayportfolio/static/media/ssn_logo.80d4b34c.png"
+  },
+  {
+    "revision": "e6d1107938472241f46877f3b2559a7d",
+    "url": "/ibakshayportfolio/static/media/ssn_logo2.e6d11079.png"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/ibakshayportfolio/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/ibakshayportfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "b48e86c9a956d3a3a32ca4a5ae05ddc7",
+    "url": "/ibakshayportfolio/static/media/talksCardBackAlt.b48e86c9.svg"
+  }
+]);
